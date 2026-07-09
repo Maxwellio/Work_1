b69cf00d5d1a609ff38efb04504fd9d77054e60d
Maxwellio <pfcmax@mail.ru>
2026-07-09 16:07:10 +0500

    Create index.d.ts

---
+++ frontend/sr/index.d.ts
@@ -0,0 +1,5 @@
+export { DynamicDateList, DynamicDatePicker, CheckBox, DebouncedInput, DynamicSelect } from './src/Input/InputComponents';
+export { BaseTreeTable } from './src/BaseTable/BaseTreeTable';
+export { BaseTable } from './src/BaseTable/BaseTable';
+export { TableFilter } from './src/BaseFilter/BaseFilter';
+export { ResizableTables } from './src/BaseTable/ResizableTables';
