8d6aa73353d0b53d83f64a3c6e096c625b672d6a
Maxwellio <pfcmax@mail.ru>
2026-07-24 16:04:37 +0500

    Update useFetchSelectContent.tsx

--- frontend/sr/input/hooks/useFetchSelectContent.tsx
+++ frontend/sr/input/hooks/useFetchSelectContent.tsx
@@ -1,25 +1,24 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
+import { useApi } from "../../services/api";
 
-export const useFetchSelectContent = <T,> (baseApi, contentApi, params?) => {
+export const useFetchSelectContent = <T,> (contentApi, params?) => {
     const [content, setContent] = useState<T[]>([]);
     const [loadingContent, setLoadingContent] = useState<boolean>(false);
-
-    const fetchData = useCallback(async (params, abortSignal) => {     
+    const api = useApi();   
+    const fetchData = useCallback(async (params, abortSignal) => {
         if (loadingContent) return;
         setLoadingContent(true);
         try {
             const reqApi = contentApi;
-            console.log(reqApi, params);    
                        
-            const data = (await baseApi.get(reqApi, {params, signal: abortSignal }));
+            const data = (await api.get(reqApi, {params, signal: abortSignal }));
             if (data.status !== 200){
                 throw new Error("Ошибка загрузки")
             }
             setContent(data.data);
         } catch(err){
             if (axios.isCancel(err)) {
-                console.log("Запрос отменен");
             } else {
                 console.error(err.message);
             }
